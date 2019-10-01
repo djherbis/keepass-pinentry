59f7f552f87d56b7b1c7264d7c6a0c7a12e8d293
Konstantin Nikulin <apollo2k4@narod.ru>
2019-10-01 07:59:38 +0000

    type fix

--- createcert.cmd
+++ createcert.cmd
@@ -1,4 +1,4 @@
 :: Creates the cert
 if not exist "%~dp0certificate.pem" openssl req -newkey rsa:4096 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
 :: Save output files & pwd in keepass under TLSKEY
-if not exist "%~certificate.p12" openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12
+if not exist "%~dp0certificate.p12" openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12
